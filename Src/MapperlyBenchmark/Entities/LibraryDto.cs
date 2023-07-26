ab672c0882b5edf502183da3a1651bf45a6e29ce
vadzim-z <vadzim.zylevich@maersk.com>
2023-07-26 16:54:22 +0200

    Refactoring

--- Src/MapperlyBenchmark/Entities/LibraryDto.cs
+++ Src/MapperlyBenchmark/Entities/LibraryDto.cs
@@ -3,5 +3,5 @@
 public class LibraryDto
 {
     public long Id { get; set; }
-    public ICollection<BookCase> BookCases { get; set; } = Array.Empty<BookCase>();
+    public IEnumerable<BookCase> BookCases { get; set; }
 }
